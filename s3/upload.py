f03e3a72ee1fc245ed5d83d023e0d227d1cd60b2
root <root@Reidner.>
2025-09-18 09:24:22 -0300

    primeiro commit

---
+++ s3/upload.py
@@ -0,0 +1,9 @@
+import boto3
+s3api = boto3.client("s3", region_name="us-east-1")
+bucket_name = "reidnerfausto2002"
+
+file_name = "./s3/index.html"
+object_name = "index.html"
+
+s3api.upload_file(file_name, bucket_name, object_name)
+print("Arquivo enviado com sucesso para o S3")
